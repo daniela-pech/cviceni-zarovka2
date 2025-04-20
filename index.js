88c0c1fea0ce04de05853aeac1bf1d35d729046d
Daniela Pech <randakova.daniela@gmail.com>
2025-04-20 22:46:55 +0200

    vypracovane cviceni, udalosti, keydown, toggle

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('funguju!');
+
+const zarovka = document.querySelector('.bulb');
+const rozsvit = () => {
+  zarovka.classList.toggle('bulb--on');
+};
+
+document.body.addEventListener('keydown', rozsvit);
